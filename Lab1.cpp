40ca8f09c5f93bfccae7abaee647b9d189f94b9d
disyled25 <nazarow.george@gmail.com>
2022-09-14 17:42:04 +0300

    Create Lab1.cpp

---
+++ Lab1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+string ReadFile()
+{
+    string line;
+    ifstream txt("D:\word.txt");
+    getline(txt, line);
+    txt.close();
+    return line;
+}
+
+vector<string> decode(string line)
+{
+    istringstream input{ line };
+    vector<string> words;
+
+    while (!input.eof())
+    {
+        string temp;
+        input >> temp;
+        words.push_back(temp);
+    }
+
+    for (int i = 0; i < words.size(); i++)
+    {
+        rotate(words[i].begin(), words[i].begin() + (words[i].length() - 3), words[i].end());
+        cout << words[i] << " ";
+    }
+
+    return words;
+}
+
+int main()
+{
+    decode(ReadFile());
+}
